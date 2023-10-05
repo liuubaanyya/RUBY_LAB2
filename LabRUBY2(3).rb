9e32f4b7d98efdd063d7b5a4314414dd9d2dc791
Liubov Prykhodko <113430539+liuubaanyya@users.noreply.github.com>
2023-10-06 00:40:40 +0300

    Add files via upload

---
+++ LabRUBY2(3).rb
@@ -0,0 +1,4 @@
+binary_number = "10000000001"
+decimal_number = binary_number.to_i(2)
+
+puts "Двійкове число #{binary_number} у десятковій системі: #{decimal_number}"
---
+++ LabRUBY2(4).rb
@@ -0,0 +1,4 @@
+decimal_number = 828
+binary_number = decimal_number.to_s(2)
+
+puts "Десяткове число #{decimal_number} у двійковій системі: #{binary_number}"
